6ee4c3cc4be90f5726dc4cf0148757c2791188e5
Kiyohiko Heima <piyo326@gmail.com>
2017-11-28 00:02:22 +0900

    Lint-fix datatypes/push.js

--- src/core/datatypes/push.js
+++ src/core/datatypes/push.js
@@ -1,37 +1,36 @@
 export default class {
-	constructor() {
-	}
+  constructor () {
+  }
 
-	static decode(message, type) {
-		if(type == 'json') return this.json(message);
-		else if(type == 'text') return this.text(message);
-		else if(type == 'binary') return this.binary(message);
-		else return this.text(message);
-	}
+  static decode (message, type) {
+    if (type === 'json') return this.json(message)
+    else if (type === 'text') return this.text(message)
+    else if (type === 'binary') return this.binary(message)
+    else return this.text(message)
+  }
 
-	static json(message) {
-		if(!message) return null;
-		let value = null;
-		try {
-			value = JSON.parse(message.v);
-		}catch(e) {
-			//JSON parse error
-			value = 'invalid json';
-		}
-		return {
-			id: message.id,
-			timestamp: Math.floor(message.t),
-			value: value
-		}
-	}
+  static json (message) {
+    if (!message) return null
+    let value = null
+    try {
+      value = JSON.parse(message.v)
+    } catch (e) {
+      // JSON parse error
+      value = 'invalid json'
+    }
+    return {
+      id: message.id,
+      timestamp: Math.floor(message.t),
+      value: value
+    }
+  }
 
-	static text(message) {
-		return message;
-	}
+  static text (message) {
+    return message
+  }
 
-	static binary(message) {
-		//TODO
-		return message;
-	}
-
+  static binary (message) {
+    // TODO
+    return message
+  }
 }
