33a86d7cfcca77e51dd6dc5dd4a47f0bd5b5ba7a
Kiyohiko Heima <piyo326@gmail.com>
2017-11-27 21:32:32 +0900

    Lint-fix src/cache

--- src/cache/index.js
+++ src/cache/index.js
@@ -1,39 +1,39 @@
-import Span from './span';
+import Span from './span'
 
-//desc cache
+// desc cache
 export default class {
-	constructor(options) {
-		this.spanList = [];
-	}
+  constructor (options) {
+    this.spanList = []
+  }
 
-	add(ts, data) {
-		let span = new Span(ts, data);
-		this.spanList.push(span);
-		this._combine();
-	}
+  add (ts, data) {
+    let span = new Span(ts, data)
+    this.spanList.push(span)
+    this._combine()
+  }
 
-	_combine() {
-		this.spanList = this.spanList.reduce((acc, s) => {
-			if(acc.length == 0) {
-				return [s];
-			}else{
-				if(acc[0].combine(s)) {
-					return acc;
-				}else{
-					return acc.concat([s]);
-				}
-			}
-		}, []);
-	}
+  _combine () {
+    this.spanList = this.spanList.reduce((acc, s) => {
+      if (acc.length === 0) {
+        return [s]
+      } else {
+        if (acc[0].combine(s)) {
+          return acc
+        } else {
+          return acc.concat([s])
+        }
+      }
+    }, [])
+  }
 
-	query(ts, limit) {
-		for(var i=0;i < this.spanList.length;i++) {
-			let s = this.spanList[i];
-			let data = s.query(ts, limit)
-			if(data !== null) {
-				return data;
-			}
-		}
-		return null;
-	}
+  query (ts, limit) {
+    for (var i = 0; i < this.spanList.length; i++) {
+      let s = this.spanList[i]
+      let data = s.query(ts, limit)
+      if (data !== null) {
+        return data
+      }
+    }
+    return null
+  }
 }
