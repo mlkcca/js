e4da630aa8cbabd7bcdea00b01183f7f48706c71
Kiyohiko Heima <piyo326@gmail.com>
2017-11-27 17:54:49 +0900

    Fix lint error at test/index.js

--- test/index.js
+++ test/index.js
@@ -36,12 +36,12 @@ describe('milkcocoa', function () {
       it('cache', function (done) {
         var ds = milkcocoa.dataStore('demo', {datatype: 'json'})
 
-        ds.history({ts: 1493027628099, limit: 2, useCache: true}, function (err, messages) {
-          ds.history({ts: 1493027628099, limit: 2, useCache: true}, function (err, messages) {
-            console.log(err, messages)
-            assert.equal(null, err)
-            assert.equal(true, messages.length === 2)
-            assert.equal(true, messages[0].hasOwnProperty('value'))
+        ds.history({ts: 1493027628099, limit: 2, useCache: true}, function (err1, messages1) {
+          ds.history({ts: 1493027628099, limit: 2, useCache: true}, function (err2, messages2) {
+            console.log(err2, messages2)
+            assert.equal(null, err2)
+            assert.equal(true, messages2.length === 2)
+            assert.equal(true, messages2[0].hasOwnProperty('value'))
             done()
           })
         })
