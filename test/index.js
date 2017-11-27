37fa698a89425aa8a0beb90c8d4a20915060c709
Kiyohiko Heima <piyo326@gmail.com>
2017-11-27 17:44:41 +0900

    Lint-fix test/index.js

--- test/index.js
+++ test/index.js
@@ -1,75 +1,65 @@
-const assert = require("assert");
-const Milkcocoa = require('../lib/node');
+const assert = require('assert')
+const Milkcocoa = require('../lib/node')
 
-var milkcocoa = new Milkcocoa({appId: 'demo', uuid: 'uuid1', apiKey: 'demo'});
+var milkcocoa = new Milkcocoa({appId: 'demo', uuid: 'uuid1', apiKey: 'demo'})
 
-describe("milkcocoa", function() {
+describe('milkcocoa', function () {
+  it('getAppId', function () {
+    assert.equal('demo', milkcocoa.getAppId())
+  })
 
-    it("getAppId", function() {
-        assert.equal('demo', milkcocoa.getAppId());
-    });
+  describe('datastore', function () {
+    describe('history', function () {
+      this.timeout(3000)
 
-	describe("datastore", function() {
-		describe("history", function() {
-			this.timeout(3000);
+      it('no option', function (done) {
+        var ds = milkcocoa.dataStore('demo', {datatype: 'json'})
 
-		    it("no option", function(done) {
-				var ds = milkcocoa.dataStore('demo', {datatype: 'json'});
+        ds.history({}, function (err, messages) {
+          assert.equal(null, err)
+          assert.equal(true, messages.length > 0)
+          done()
+        })
+      })
 
-				ds.history({}, function(err, messages) {
-		        	assert.equal(null, err);
-		        	assert.equal(true, messages.length>0);
-					done();
-				});
-		    });
+      it('ts', function (done) {
+        var ds = milkcocoa.dataStore('demo', {datatype: 'json'})
 
-		    it("ts", function(done) {
-				var ds = milkcocoa.dataStore('demo', {datatype: 'json'});
+        ds.history({ts: 1493027628099, limit: 2}, function (err, messages) {
+          console.log(err, messages)
+          assert.equal(null, err)
+          assert.equal(true, messages.length === 2)
+          done()
+        })
+      })
 
-				ds.history({ts:1493027628099,limit:2}, function(err, messages) {
-					console.log(err, messages);
-		        	assert.equal(null, err);
-		        	assert.equal(true, messages.length==2);
-					done();
-				});
-		    });
+      it('cache', function (done) {
+        var ds = milkcocoa.dataStore('demo', {datatype: 'json'})
 
+        ds.history({ts: 1493027628099, limit: 2, useCache: true}, function (err, messages) {
+          ds.history({ts: 1493027628099, limit: 2, useCache: true}, function (err, messages) {
+            console.log(err, messages)
+            assert.equal(null, err)
+            assert.equal(true, messages.length === 2)
+            assert.equal(true, messages[0].hasOwnProperty('value'))
+            done()
+          })
+        })
+      })
+    })
 
-		    it("cache", function(done) {
-				var ds = milkcocoa.dataStore('demo', {datatype: 'json'});
+    describe('send', function () {
+      this.timeout(3000)
 
-				ds.history({ts:1493027628099,limit:2,useCache:true}, function(err, messages) {
-					ds.history({ts:1493027628099,limit:2,useCache:true}, function(err, messages) {
-						console.log(err, messages);
-			        	assert.equal(null, err);
-			        	assert.equal(true, messages.length==2);
-			        	assert.equal(true, messages[0].hasOwnProperty('value'));
-						done();
-					});
-				});
-		    });
+      it('success to send data', function (done) {
+        var ds = milkcocoa.dataStore('topic', {datatype: 'json'})
 
-		});
-
-
-		describe("send", function() {
-			this.timeout(3000);
-
-		    it("success to send data", function(done) {
-				var ds = milkcocoa.dataStore('topic', {datatype: 'json'});
-
-				ds.on('send', function(data) {
-		        	assert.deepEqual({value:{message:'Hello'}}, data);
-					done();
-				});
-				ds.send({message:'Hello'});
-		    });
-
-		});
-
-	});
-
-});
-
-
-
+        ds.on('send', function (data) {
+          assert.deepEqual({value: {message: 'Hello'}}, data)
+          done()
+        })
+        ds.send({message: 'Hello'})
+      })
+    })
+  })
+})
